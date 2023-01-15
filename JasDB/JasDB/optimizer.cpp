0331299fa4a8b880a51cc08cc0903052e450486c
heyulan0112 <heyulan0112@gmail.com>
2023-01-15 16:50:52 -0500

    JasDB add optimizer module

--- JasDB/JasDB/optimizer.cpp
+++ JasDB/JasDB/optimizer.cpp
@@ -6,3 +6,224 @@
 //
 
 #include "optimizer.hpp"
+#include <iostream>
+using namespace hsql;
+
+namespace jasdb{
+
+Plan* Optimizer::create_plan_tree(const SQLStatement* stmt){
+    switch (stmt->type()) {
+        case kStmtSelect:
+            return create_select_plan_tree(static_cast<const SelectStatement*>(stmt));
+        case kStmtInsert:
+            return create_insert_plan_tree(static_cast<const InsertStatement*>(stmt));
+        case kStmtUpdate:
+            return create_update_plan_tree(static_cast<const UpdateStatement*>(stmt));
+        case kStmtDelete:
+            return create_delete_plan_tree(static_cast<const DeleteStatement*>(stmt));
+        case kStmtCreate:
+            return create_create_plan_tree(static_cast<const CreateStatement*>(stmt));
+        case kStmtDrop:
+            return create_drop_plan_tree(static_cast<const DropStatement*>(stmt));
+        case kStmtTransaction:
+            return create_trx_plan_tree(static_cast<const TransactionStatement*>(stmt));
+        case kStmtShow:
+            return create_show_plan_tree(static_cast<const ShowStatement*>(stmt));
+        default:
+            std::cout << "Statement type is not supported." << std::endl;
+    }
+    return NULL;
+}
+
+Plan* Optimizer::create_create_plan_tree(const CreateStatement *stmt){
+    CreatePlan* plan = new CreatePlan(stmt->type);
+    plan->ifNotExist = stmt->ifNotExists;
+    plan->type = stmt->type;
+    plan->schema = stmt->schema;
+    plan->tableName = stmt->tableName;
+    plan->indexName = stmt->indexName;
+    plan->columns = stmt->columns;
+    plan->next = NULL;
+    
+    // Create index for an existed table
+    if(plan->type == kCreateIndex){
+        Table* table = global_meta_data.get_table(plan->schema, plan->tableName);
+        if(table == NULL){
+            std::cout << "Table not found." << std::endl;
+            delete plan;
+            return NULL;
+        }
+        if(stmt->indexColumns != NULL){
+            plan->indexColumns = new std::vector<ColumnDefinition*>;
+        }
+        for(auto column_name : *stmt->indexColumns){
+            ColumnDefinition* col_def = table->get_column(column_name);
+            if(col_def == NULL){
+                std::cout << "Column not found." << std::endl;
+                delete plan->indexColumns;
+                delete plan;
+                return NULL;
+            }
+            plan->indexColumns->push_back(col_def);
+        }
+        return plan;
+    }
+    
+    return plan;
+}
+
+Plan* Optimizer::create_drop_plan_tree(const DropStatement *stmt){
+    DropPlan* plan = new DropPlan();
+    plan->type = stmt->type;
+    plan->ifExists = stmt->ifExists;
+    plan->schema = stmt->schema;
+    plan->name = stmt->name;
+    plan->indexName = stmt->indexName;
+    plan->next = NULL;
+    return plan;
+}
+
+Plan* Optimizer::create_insert_plan_tree(const InsertStatement *stmt){
+    InsertPlan* plan = new InsertPlan();
+    plan->type = stmt->type;
+    plan->table = global_meta_data.get_table(stmt->schema, stmt->tableName);
+    plan->values = stmt->values;
+    return plan;
+}
+
+Plan* Optimizer::create_update_plan_tree(const UpdateStatement *stmt){
+    Table* table = global_meta_data.get_table(stmt->table->schema, stmt->table->name);
+    ScanPlan* scan = new ScanPlan();
+    scan->type = kSeqScan;
+    scan->table = table;
+    Plan* plan = scan;
+    if(stmt->where != NULL){
+        Plan* filter = create_filter_plan_tree(table->columns, stmt->where);
+        filter->next = plan;
+        plan = filter;
+    }
+    UpdatePlan* update_plan = new UpdatePlan();
+    update_plan->table = table;
+    // Update->Filter->Scan
+    update_plan->next = plan;
+
+    for(auto update : *stmt->updates){
+        size_t idx = 0;
+        update_plan->values.push_back(update->value);
+        for(auto col : table->columns){
+            // char* string
+            if(update->column == col->name){
+                // which index to be updated
+                update_plan->indexes.push_back(idx);
+                break;
+            }
+            idx++;
+        }
+    }
+    
+    return update_plan;
+}
+
+Plan* Optimizer::create_delete_plan_tree(const DeleteStatement *stmt){
+    Table* table = global_meta_data.get_table(stmt->schema, stmt->tableName);
+    
+    ScanPlan* scan = new ScanPlan();
+    scan->type = kSeqScan;
+    scan->table = table;
+    Plan* plan = scan;
+    
+    if(stmt->expr != NULL){
+        Plan* filter = create_filter_plan_tree(table->columns, stmt->expr);
+        filter->next = plan;
+        plan = filter;
+    }
+    
+    DeletePlan* del_plan = new DeletePlan();
+    del_plan->table = table;
+    // Delete->Filter->Scan
+    del_plan->next = plan;
+    return del_plan;
+}
+
+Plan* Optimizer::create_select_plan_tree(const SelectStatement *stmt){
+    Table* table = global_meta_data.get_table(stmt->fromTable->schema, stmt->fromTable->name);
+    std::vector<ColumnDefinition*> columns = table->columns;
+    ScanPlan* scan = new ScanPlan();
+    scan->type = kSeqScan;
+    scan->table = table;
+    Plan* plan = scan;
+    if(stmt->whereClause != NULL){
+        Plan* filter = create_filter_plan_tree(columns, stmt->whereClause);
+        filter->next = plan;
+        plan = filter;
+    }
+    SelectPlan* select_plan = new SelectPlan();
+    select_plan->table = table;
+    // Select->Filter->Scan
+    select_plan->next = plan;
+    
+    for(auto expr:*stmt->selectList){
+        if(expr->type == kExprStar){
+            // select *
+            for(size_t i=0;i<columns.size();i++){
+                ColumnDefinition* col_def = columns[i];
+                select_plan->out_cols.push_back(col_def);
+                select_plan->col_ids.push_back(i);
+            }
+        }
+        else{
+            for(size_t i=0;i<columns.size();i++){
+                // select col_name1, col_name2, ...
+                ColumnDefinition* col_def = columns[i];
+                if(strcmp(expr->name, col_def->name) == 0){
+                    select_plan->out_cols.push_back(col_def);
+                    select_plan->col_ids.push_back(i);
+                }
+            }
+        }
+    }
+    return select_plan;
+}
+
+// used to 'where' like "where id = 1"
+// FilterPlan contains the filter information
+Plan* Optimizer::create_filter_plan_tree(std::vector<ColumnDefinition *> columns, Expr *where){
+    FilterPlan* filter = new FilterPlan();
+    Expr* col = NULL;
+    Expr* val = NULL;
+    if(where->expr->type == kExprColumnRef){
+        col = where->expr;
+        val = where->expr2;
+    }
+    else{
+        col = where->expr2;
+        val = where->expr;
+    }
+    // get the index of column which need to be filtered out
+    for(auto i=0;i<columns.size();i++){
+        ColumnDefinition* col_def = columns[i];
+        if(strcmp(col->name, col_def->name) == 0){
+            filter->idx = i;
+        }
+    }
+    filter->val = val;
+    return filter;
+}
+
+Plan* Optimizer::create_trx_plan_tree(const TransactionStatement *stmt){
+    TrxPlan* plan = new TrxPlan();
+    plan->command = stmt->command;
+    return plan;
+}
+
+Plan* Optimizer::create_show_plan_tree(const ShowStatement *stmt){
+    ShowPlan* plan = new ShowPlan();
+    plan->type = stmt->type;
+    plan->schema = stmt->schema;
+    plan->name = stmt->name;
+    plan->next = NULL;
+    return plan;
+}
+
+}
+
--- JasDB/JasDB/optimizer.hpp
+++ JasDB/JasDB/optimizer.hpp
@@ -9,5 +9,158 @@
 #define optimizer_hpp
 
 #include <stdio.h>
+#include <cstring>
+#include "sql-parser-master/src/sql/statements.h"
+#include "metadata.hpp"
+using namespace hsql;
+
+namespace jasdb{
+enum planType{
+    kCreate,
+    kDrop,
+    kInsert,
+    kUpdate,
+    kDelete,
+    kSelect,
+    kScan,
+    kProjection,
+    kFilter,
+    kSort,
+    kLimit,
+    kTrx,
+    kShow
+};
+
+class Plan {
+public:
+    planType plan_type;
+    Plan* next;
+    Plan(planType t){
+        plan_type = t;
+        next = NULL;
+    }
+    ~Plan(){
+        delete next;
+        next = NULL;
+    }
+};
+
+class CreatePlan : public Plan{
+public:
+    CreateType type; // to specify what kind of create create index table or schema
+    bool ifNotExist;
+    char* schema;
+    char* tableName;
+    char* indexName;
+    std::vector<ColumnDefinition*>* indexColumns;
+    std::vector<ColumnDefinition*>* columns;
+    CreatePlan(CreateType t) : Plan(kCreate){
+        type = t;
+    }
+};
+
+class DropPlan : public Plan{
+public:
+    DropType type;
+    bool ifExists;
+    char* schema;
+    char* name;
+    char* indexName;
+    DropPlan() : Plan(kDrop){}
+};
+
+class InsertPlan : public Plan {
+public:
+    InsertType type;
+    Table* table;
+    std::vector<Expr*>* values;
+    InsertPlan() : Plan(kInsert){}
+};
+
+class UpdatePlan : public Plan {
+public:
+    Table* table;
+    std::vector<Expr*> values;
+    std::vector<size_t> indexes;
+    UpdatePlan() : Plan(kUpdate){}
+};
+
+class DeletePlan : public Plan {
+public:
+    Table* table;
+    DeletePlan() : Plan(kDelete){}
+};
+
+class SelectPlan : public Plan {
+public:
+    Table* table;
+    std::vector<ColumnDefinition*> out_cols;
+    std::vector<size_t> col_ids;
+    SelectPlan() : Plan(kSelect) {}
+};
+
+enum scanType {
+    kSeqScan,
+    kIndexScan
+};
+
+class ScanPlan : public Plan {
+public:
+    scanType type;
+    Table* table;
+    ScanPlan() : Plan(kScan){}
+};
+
+class FilterPlan : public Plan {
+public:
+    size_t idx;
+    Expr* val;
+    FilterPlan() : Plan(kFilter), idx(0), val(NULL){}
+};
+
+class SortPlan : public Plan {
+public:
+    Table* table;
+    std::vector<OrderDescription*> order;
+    SortPlan() : Plan(kSort){}
+};
+
+class LimitPlan : public Plan {
+public:
+    LimitPlan() : Plan(kLimit) {}
+    int offset;
+    int limit;
+};
+
+class TrxPlan : public Plan {
+public:
+    TrxPlan() : Plan(kTrx){}
+    TransactionCommand command;
+};
+
+class ShowPlan : public Plan {
+public:
+    ShowType type;
+    std::string schema;
+    std::string name;
+    ShowPlan() : Plan(kShow){}
+};
+
+class Optimizer {
+public:
+    Optimizer(){}
+    Plan* create_plan_tree(const SQLStatement* stmt);
+    Plan* create_create_plan_tree(const CreateStatement* stmt);
+    Plan* create_drop_plan_tree(const DropStatement* stmt);
+    Plan* create_insert_plan_tree(const InsertStatement* stmt);
+    Plan* create_update_plan_tree(const UpdateStatement* stmt);
+    Plan* create_delete_plan_tree(const DeleteStatement* stmt);
+    Plan* create_select_plan_tree(const SelectStatement* stmt);
+    Plan* create_filter_plan_tree(std::vector<ColumnDefinition*> columns, Expr* where);
+    Plan* create_trx_plan_tree(const TransactionStatement* stmt);
+    Plan* create_show_plan_tree(const ShowStatement* stmt);
+};
+
+}
 
 #endif /* optimizer_hpp */
